119f0490ea68607f1c500237f7dabc7524fd409c
jcontour <contj186@newschool.edu>
2019-03-28 17:53:06 -0400

    animation

---
+++ Animator/Assets/AnimateMe.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateMe : MonoBehaviour
+{
+
+    public GameObject player;
+    Animator anim;
+    float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, player.transform.position);
+        anim.SetFloat("makeColorChange", dist);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        anim.SetTrigger("makeShrink");
+    }
+
+}
